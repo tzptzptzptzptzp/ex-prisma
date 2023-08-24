71d800251738cb6a5cf784067238d7301e6d7be4
tzptzptzptzptzp <tzp2539@gmail.com>
2023-08-25 00:46:17 +0900

    ✨ データを追加するAPIを実装

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 // ポート番号の定義
 const PORT = 8000;
 
 app.use(express.json());
+
+app.post("/", async (req, res) => {
+  const { title, body } = req.body;
+  const posts = await prisma.posts.create({
+    data: {
+      title: title,
+      body: body,
+    },
+  });
+  return res.json(posts);
+});
 
 app.listen(PORT, () => {
   console.log(`Server Running http://localhost:${PORT}`);
